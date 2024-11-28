416565b6176b39c3c19e2471857636070d58fdee
billyistyping000 <bcengac@gmail.com>
2024-11-28 16:05:02 +0800

    28NovPM, little adjustment

--- Movie-express/src/components/Navbar/Navbar.jsx
+++ Movie-express/src/components/Navbar/Navbar.jsx
@@ -5,45 +5,44 @@ export default function Navbar() {
     <>
       <div className="navbar-container">
         <nav class="navbar-top">
+          <input class="search-box" type="text" placeholder="Search Movies" />
+          <div class="nav-mid"></div>
           <div id="nav-logo">This.Movie</div>
-          <ul class="nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                TV Shows
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Moives
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Channels
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Recently Watched
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                My List
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Adult
-              </a>
-            </li>
-          </ul>
         </nav>
+        {/* 分類 */}
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              TV Shows
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Moives
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Channels
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              My List
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Adult
+            </a>
+          </li>
+        </ul>
       </div>
     </>
   );
