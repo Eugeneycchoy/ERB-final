d535dae069054f5f56379b8083ba6fe3a1b4637b
billyistyping000 <bcengac@gmail.com>
2024-11-28 14:37:57 +0800

    navbar idea

--- Movie-express/src/components/Navbar/Navbar.jsx
+++ Movie-express/src/components/Navbar/Navbar.jsx
@@ -4,8 +4,45 @@ export default function Navbar() {
   return (
     <>
       <div className="navbar-container">
-        <nav>
-          <h2>Navbar Section</h2>
+        <nav class="navbar-top">
+          <div id="nav-logo">This.Movie</div>
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                TV Shows
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Moives
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Channels
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Recently Watched
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                My List
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Adult
+              </a>
+            </li>
+          </ul>
         </nav>
       </div>
     </>
