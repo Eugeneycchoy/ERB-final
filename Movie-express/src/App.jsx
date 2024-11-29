b9d576cded1902e41821a273a1d3086c32ecacc2
billyistyping000 <bcengac@gmail.com>
2024-11-29 10:44:22 +0800

    updated css
    navbar layout& hover layout&
    added bento under movie carosel

--- Movie-express/src/App.jsx
+++ Movie-express/src/App.jsx
@@ -49,6 +49,18 @@ function App() {
       <Navbar />
       <main>
         <MovieCarousel movies={popularMovies} tag="🔥 Now Trending" />
+        {/* Bento */}
+        <div className="movie-bento-container">
+          <div className="nowplaying-container">
+            <h2>Now Playing </h2>
+          </div>
+          <div className="recently-container">
+            <h2>Continue Watching</h2>
+          </div>
+          <div className="recommend-container">
+            <h2>You Might Like</h2>
+          </div>
+        </div>
       </main>
     </>
   );
